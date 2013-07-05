5dfb9c582569ec4158b30475a9d8ea800aa7a632
nanke <xiongpingwu@gmail.com>
2013-07-05 16:44:35 +0800

    444

--- Teamwork/Login.aspx.cs
+++ Teamwork/Login.aspx.cs
@@ -16,30 +16,34 @@ namespace Teamwork
 
         }
 
-  
+   
 
         protected void Button1_Click1(object sender, EventArgs e)
         {
 
-            //SqlConnection connection = new SqlConnection(System.Web.Configuration.WebConfigurationManager.ConnectionStrings["SampleConnectionString"].ConnectionString.ToString());
-            //connection.Open();
-            //SqlCommand myCommand = new SqlCommand("select * from User", connection);
+            //SqlConnection myConnection = new SqlConnection(System.Web.Configuration.WebConfigurationManager.ConnectionStrings["SampleConnectionString"].ConnectionString.ToString());
+            //myConnection.Open();
+            //SqlCommand myCommand = new SqlCommand("select * from LoginUser", myConnection);
             //SqlDataReader myReader = myCommand.ExecuteReader();
-            //string a = "";
+            //string a = TextBox1.Text;
+            //string b = TextBox2.Text;
             //while (myReader.Read())
             //{
-   
-            //        if (TextBox1.Text == myReader["Name"].ToString() && TextBox2.Text == myReader["Password"].ToString())
+            //    Button1.Text = a+"  "+ myReader["Name"].ToString() +"  "+b+" "+ myReader["Password"].ToString();
+            //        if (a == myReader["Name"].ToString() && b.ToString() == myReader["Password"].ToString())
             //        {
+            //            Button1.Text = "good";
             //            if (myReader["UserType"] == "Student") Response.Redirect("ShowQuestionList.aspx");
-            //            else Response.Redirect("ReplyQusetionList.aspx");
+            //            else Response.Redirect("ReplyQuestionList.aspx");
             //        }
         
 
 
             //}
-            if (TextBox1.Text== "请叫我欢娘") Response.Redirect("ShowQuestionList.aspx");
-            if (TextBox1.Text == "请叫我靖哥哥") Response.Redirect("ReplyQuestionList.aspx");
+
+            //因为上述代码有些问题，解决不了，所以先静态判断
+            if (TextBox1.Text== "请叫我欢娘" && TextBox2.Text=="1234") Response.Redirect("ShowQuestionList.aspx");
+            if (TextBox1.Text == "请叫我军哥" && TextBox2.Text == "1234") Response.Redirect("ReplyQuestionList.aspx");
 
         }
 
